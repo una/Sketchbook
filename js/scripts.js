1815eba88d0d57a7e6809e454c800492a829e3c1
Una Kravets <Una21136@yahoo.com>
2013-11-02 17:53:05 -0400

    scripts file added

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var feed = new Instafeed({
+	get: 'tagged',
+	tagName: 'unasketch',
+	resolution: 'standard_resolution',
+	clientId: 'b9bc28b31ab44a399a0d1399c09edd70',
+	template: '<li class="sketch"><a href="{{link}}"><img src="{{image}}" /></a></li>'
+});
+feed.run();
